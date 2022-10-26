1b23d9a68bfed912200a8aff063f9a03b223c384
ludexab <ileanwaabalaka@gmail.com>
2022-10-26 11:13:14 +0100

    minor bugs

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,6 @@ import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 import { TransactionContext } from "../context/TransactionContext";
-//import { shortenAddress } from "../utils/shortenAddress"
 
 const commonStyles =
   "text-white min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
